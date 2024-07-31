490d4c8dee8cd0232fc791ac8d472f2c656a5f91
tech-immersion <techimmersionworkshop@gmail.com>
2024-07-31 17:06:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-import logo from './logo.svg';
-import LocateNearestHospital from './pages/LocateNearestHospital.js';
-import AdminLogin  from './pages/AdminLogin.js';
-import Crud from './pages/Crud.js';
-import Home from './pages/Home.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CreateAccount from './pages/CreateAccount.js';
 import LocateNearestHospitalMyLoc from './pages/LocateNearestHospitalMyLoc.js';
 
 function App() {
