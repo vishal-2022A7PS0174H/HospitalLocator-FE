77dc1e3c8b426aecb14fe1803be41af7cc9b555f
vishal-2022A7PS0174H <144363866+vishal-2022A7PS0174H@users.noreply.github.com>
2024-09-11 03:49:03 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66c5f60ea11d2998621456epyb5cb49";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-cloud-user-18.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
